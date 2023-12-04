6842d3c0989d59edd612329e2b6690589f1da5f8
Lu27656348 <luiscarlossomoza@gmail.com>
2023-12-03 22:31:36 -0400

    fix(main): Arreglar recorrido en rombo
    Arreglar recorrido en rombo

--- prubeTabla.py
+++ prubeTabla.py
@@ -1,19 +1,22 @@
 N=int(input('ingrese N: '))
 tabla=[[None for i in range(N)] for j in range(N)]
-BordeIzq = N//2 - 1
-BordeDer = N//2 + 1
+BordeIzq = (N//2)
+BordeDer = (N//2)
 k=1
+tabla[0][N//2] = 0;
+for i in range(1,N-1):
+    if(i<=N//2):
+        for j in range((BordeIzq-i),(BordeDer+i)+1):
+            tabla[i][j] = k
+            k = k + 1
+    else:
+        BordeIzq = N//2
+        BordeDer = (N-1)
+        for j in range(i-BordeIzq,BordeDer):
+            tabla[i][j]=k
+            BordeDer = BordeDer - 1
+            k=k+1
 
-for i in range(N//2 + 1):
-    for j in range((BordeIzq-i),(BordeDer+i)):
-        tabla[i][j] = k
-        k = k + 1
-
-contBorde=0
-
-for i in range(N//2 + 2):
-    contBorde= contBorde + 1
-    for j in range((BordeIzq+contBorde),(BordeDer-contBorde)):
-        tabla[i][j]=k
-        k=k+1
-print(tabla)
+tabla[N-1][N//2] =k;
+for fila in tabla:
+    print(fila)
